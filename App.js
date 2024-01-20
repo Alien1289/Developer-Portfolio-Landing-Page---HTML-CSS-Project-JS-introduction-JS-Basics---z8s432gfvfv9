c0980cb632baa8abf282142dbfd895a4beb64d31
Alien1289 <pranavarshn2000@gmail.com>
2024-01-20 18:24:52 +0530

    code

--- App.js
+++ App.js
@@ -33,7 +33,9 @@ function attachingEventsToDiffElements(element, elementBorder) {
     }
 }
 
-
+const viewportHeight = window.innerHeight;
+const viewportWidth = window.innerWidth;
+document.getElementById('section-hero-container').style['min-height'] = `${viewportHeight - 80}px`
 
 
 document.getElementsByClassName('about-me-info-list-item-skills')[0].addEventListener('click', function () {
@@ -55,54 +57,180 @@ document.getElementsByClassName('about-me-info-list-item-education')[0].addEvent
     document.getElementsByClassName('about-me-info-list-item-skills')[0].classList.remove('about-me-info-list-item-active')
     document.getElementsByClassName('about-me-info-list-item-experience')[0].classList.remove('about-me-info-list-item-active')
     document.getElementsByClassName('about-me-info-list-item-education')[0].classList.add('about-me-info-list-item-active')
-    document.getElementById('about-me-info-dynamic-content').innerHTML = "<span>2018-2022</span><ul><li>NIT SURAT</li></ul>"
+    document.getElementById('about-me-info-dynamic-content').innerHTML = "<span>2018-2022</span><ul><li>National Institute of Technology Surat, Gujarat</li></ul>"
 })
 
 
 
 
 for (let ind = 0; ind < document.getElementsByClassName('my-projects-items').length; ind++) {
-    projectOverlayAnimation(document.getElementsByClassName('my-projects-items')[ind], document.getElementsByClassName('my-projects-items')[ind].children[1])
+    projectOverlayAnimation(document.getElementsByClassName('my-projects-items')[ind], document.getElementsByClassName('my-projects-items')[ind].children[2])
 }
 
 function projectOverlayAnimation(element, overlayElement) {
-    let top = 451;
-    let topDecreaseInterval;
-    let topIncreaseInterval;
+    if (Number(viewportWidth) >= 640) {
+        overlayElement.style['top'] = `350px`
+        let topDecreaseInterval;
+        let topIncreaseInterval;
 
-    element.addEventListener('mouseenter', function () {
-        if (top === 451) {
-            top = 450;
-            topDecreaseInterval = setInterval(function () {
-                console.log(top)
-                overlayElement.style['top'] = `${top}px`
-                if (top === 150) {
-                    clearInterval(topDecreaseInterval)
-                } else{
-                    top -= 50
-                }
-            }, 3)
-        }
+        element.addEventListener('mouseenter', function () {
+            clearInterval(topIncreaseInterval)
+            if (Number(overlayElement.style['top'].split('px')[0]) <= 350) {
+                topDecreaseInterval = setInterval(function () {
+                    if (Number(overlayElement.style['top'].split('px')[0]) === 150) {
+                        clearInterval(topDecreaseInterval)
+                    } else {
+                        overlayElement.style['top'] = `${Number(overlayElement.style['top'].split('px')[0]) - 50}px`
+                    }
+                }, 30)
+            }
+        })
+
+        element.addEventListener('mouseleave', function () {
+            clearInterval(topDecreaseInterval)
+            if (Number(overlayElement.style['top'].split('px')[0]) >= 150) {
+                topIncreaseInterval = setInterval(function () {
+                    if (Number(overlayElement.style['top'].split('px')[0]) === 350) {
+                        clearInterval(topIncreaseInterval)
+                    } else {
+                        overlayElement.style['top'] = `${Number(overlayElement.style['top'].split('px')[0]) + 50}px`
+                    }
+                }, 30)
+            }
+
+        })
+    }else{
+        overlayElement.style['top'] = `150px`
+    }
+
+}
+
+if(Number(viewportWidth) >= 640){
+    document.getElementsByClassName('ftr-btns')[0].addEventListener('mouseenter', function(){
+        document.getElementsByClassName('ftr-btns')[0].style['background-color'] = 'white'
+    })
+    document.getElementsByClassName('ftr-btns')[0].addEventListener('mousedown', function(){
+        document.getElementsByClassName('ftr-btns')[0].style['background-color'] = '#00FF7F'
+        setTimeout(function(){
+        document.getElementsByClassName('ftr-btns')[0].style['background-color'] = 'white'
+        }, 200)
+        
+    })
+    document.getElementsByClassName('ftr-btns')[0].addEventListener('mouseleave', function(){
+        document.getElementsByClassName('ftr-btns')[0].style['background-color'] = '#00FF7F'
     })
 
-    element.addEventListener('mouseleave', function () {
-        if (top === 150) {
-            topIncreaseInterval = setInterval(function () {
-                console.log(top)
-                overlayElement.style['top'] = `${top}px`
-                if (top === 450) {
-                    clearInterval(topIncreaseInterval)
-                    top = 451
-                } else {
-                    top += 50
-                }
-            }, 3)
-        }
+    document.getElementsByClassName('ftr-btns')[1].addEventListener('mouseenter', function(){
+        document.getElementsByClassName('ftr-btns')[1].style['background-color'] = 'white'
     })
+    document.getElementsByClassName('ftr-btns')[1].addEventListener('mousedown', function(){
+        document.getElementsByClassName('ftr-btns')[1].style['background-color'] = '#00FF7F'
+        setTimeout(function(){
+        document.getElementsByClassName('ftr-btns')[1].style['background-color'] = 'white'
+        }, 200)
+        
+    })
+    document.getElementsByClassName('ftr-btns')[1].addEventListener('mouseleave', function(){
+        document.getElementsByClassName('ftr-btns')[1].style['background-color'] = '#00FF7F'
+    })
+}else{
+    document.getElementsByClassName('ftr-btns')[0].addEventListener('mousedown', function(){
+        document.getElementsByClassName('ftr-btns')[0].style['background-color'] = 'white'
+        setTimeout(function(){
+            document.getElementsByClassName('ftr-btns')[0].style['background-color'] = '#00FF7F'
+        }, 200)
+        
+    })
+}
+
+let leftHamburger = 120;
+let hamburgerStartInterval;
+let hamburgerEndInterval;
+
+document.getElementById('nav-hamburger').addEventListener('mousedown', function(){
+    clearInterval(hamburgerEndInterval)
+    clearInterval(hamburgerStartInterval)
+    document.getElementById('nav-hamburger').style['color'] = 'white'
+    setTimeout(() => {
+        document.getElementById('nav-hamburger').style['color'] = '#00FF7F'
+    }, 200);
+
+
+    document.getElementById('hamburger-menu').style['display'] = ['block']
+
+    hamburgerStartInterval = setInterval(() => {
+        if (leftHamburger === 0){
+            clearInterval(hamburgerStartInterval)
+        }else{
+            leftHamburger -= 20
+            document.getElementById('hamburger-menu').style['left'] = `${leftHamburger}vw`
+        }
+    }, 60);
+})
+
+
+document.getElementById('hamburger-menu-btn').addEventListener('mousedown', function(){
+    clearInterval(hamburgerStartInterval)
+    clearInterval(hamburgerEndInterval)
+    document.getElementById('hamburger-menu-btn').style['color'] = 'black'
+    document.getElementById('hamburger-menu-btn').style['background-color'] = 'white'
+
+    setTimeout(() => {
+        document.getElementById('hamburger-menu-btn').style['color'] = 'white'
+        document.getElementById('hamburger-menu-btn').style['background-color'] = 'black'
+    }, 200);
+
+    hamburgerEndInterval = setInterval(() => {
+        if (leftHamburger === 120){
+            clearInterval(hamburgerStartInterval)
+            document.getElementById('hamburger-menu').style['display'] = ['none']
+        }else{
+            leftHamburger += 20
+            document.getElementById('hamburger-menu').style['left'] = `${leftHamburger}vw`
+        }
+    }, 60);
+})
+
+function hamCloser(){
+    clearInterval(hamburgerStartInterval)
+    clearInterval(hamburgerEndInterval)
+    hamburgerEndInterval = setInterval(() => {
+        if (leftHamburger === 120){
+            clearInterval(hamburgerStartInterval)
+            document.getElementById('hamburger-menu').style['display'] = ['none']
+        }else{
+            leftHamburger += 20
+            document.getElementById('hamburger-menu').style['left'] = `${leftHamburger}vw`
+        }
+    }, 60);
 }
 
 
 
+document.addEventListener('scroll', function() {
+    const scrollTop = window.scrollY;
+
+    const overlay = document.getElementById('section-hero-container');
+
+    const fadeEndHeight = 200;
+
+    const maxOpacity = 0.9;
+
+    const opacity = Math.min(scrollTop / fadeEndHeight, maxOpacity);
+
+    if (scrollTop > fadeEndHeight) {
+        overlay.style.transition = 'background-color 0.3s ease';
+        overlay.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+    } else {
+        overlay.style.transition = 'background-color 0.3s ease';
+        overlay.style.backgroundColor = 'transparent';
+    }
+});
+
+
+
+
+document.getElementById('nav-logo-redirect').setAttribute('href', window.location.href)
 
 
 
@@ -118,34 +246,22 @@ function projectOverlayAnimation(element, overlayElement) {
 
 
 
+// const scriptURL = 'https://script.google.com/macros/s/AKfycby0wOpWkFonZTYmHNlRu5rz-k0G4r8zXfjdj9WA-Q6F18gy3TkXlmsksTONjrM13OiT5g/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById('msg')
 
-
-
-
-
-
-
-
-
-
-
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycby0wOpWkFonZTYmHNlRu5rz-k0G4r8zXfjdj9WA-Q6F18gy3TkXlmsksTONjrM13OiT5g/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById('msg')
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//         .then(response => {
+//             msg.innerHTML = "Message sent successfully"
+//             setTimeout(function () {
+//                 msg.innerHTML = ""
+//             }, 5000)
+//             form.reset()
+//         })
+//         .catch(error => console.error('Error!', error.message))
+// })
 
 
 
