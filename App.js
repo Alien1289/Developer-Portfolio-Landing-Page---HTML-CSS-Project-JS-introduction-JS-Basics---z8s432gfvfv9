c9c3de8c4ba876940ffcac55d325b43041f2a63d
Alien1289 <pranavarshn2000@gmail.com>
2024-01-20 21:40:26 +0530

    code

--- App.js
+++ App.js
@@ -248,27 +248,4 @@ function reloader(){
 
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycby0wOpWkFonZTYmHNlRu5rz-k0G4r8zXfjdj9WA-Q6F18gy3TkXlmsksTONjrM13OiT5g/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById('msg')
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
-
-
-
-
-
-
-
 
